677b5cc1941f3d479fc5d82e6d74c9bc73bc14a4
Ishan <129866495+ishanjaiswal146@users.noreply.github.com>
2024-10-02 00:09:30 +0530

    SQL scripts solving music store data problems.
    This repository features SQL scripts for solving music store data challenges. Includes customer analysis, sales reporting, inventory management, and data cleaning. Demonstrates SQL proficiency in extracting insights, optimizing queries, and ensuring data integrity.
    
    Project Objectives:
    -To analyze and manage music store data using SQL.
    -To extract meaningful insights and generate reports based on customer purchases, inventory, and sales performance.
    -To improve data retrieval efficiency and optimize database queries.
    
    Key Features:
    -Customer Analysis: Queries that provide insights into customer behavior, purchase history, and segmentation.
    -Sales Reporting: SQL commands to generate detailed sales reports, track revenue, and identify best-selling products.
    -Inventory Management: Scripts to manage inventory levels, identify low-stock items, and streamline reordering processes.
    -Data Cleaning: Techniques to ensure data integrity and accuracy, including handling duplicates and null values.

---
+++ Query 2.sql
@@ -0,0 +1,13 @@
+
+-- email, first name , last name and genre of music listener in alphabetically order.
+
+SELECT DISTINCT email,first_name, last_name
+FROM customer
+JOIN invoice ON customer.customer_id = invoice.customer_id
+JOIN invoice_line ON invoice.invoice_id = invoice_line.invoice_id
+WHERE track_id IN(
+	SELECT track_id FROM track
+	JOIN genre ON track.genre_id = genre.genre_id
+	WHERE genre.name LIKE 'Rock'
+)
+ORDER BY email;
